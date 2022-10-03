0d8bfdd7d3874686c58ca8e9270c94dbe5bce862
sajid <anindasajid@gmail.com>
2022-10-04 00:18:34 +0600

    top artists, search feature complete

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,5 +1,3 @@
-import axios from "axios";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Error, Loader, SongCard } from "../components";
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
